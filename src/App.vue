7ef34162ebe6cd328da0f44f87c7c3357b70430d
Dominik Jawor <jawor@nonstop.co>
2018-03-03 23:00:38 +0100

    First approaches

--- src/App.vue
+++ src/App.vue
@@ -9,29 +9,44 @@
       fixed
       app
     >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+        <v-toolbar dark class="primary">
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-title class="title">
+                  <v-icon>shopping_cart</v-icon>
+                Application
+              </v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-toolbar>
+        <v-divider></v-divider>
+
+        <v-list dense class="pt-0">
+            <v-list-tile v-for="(item, i) in items" :key="i" @click="">
+                <v-list-tile-action>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+
+        <v-divider></v-divider>
+        
+
     </v-navigation-drawer>
     <v-toolbar
-    dark
+      dark
       app
       :clipped-left="clipped"
-      class="primary">
+      class="primary"
+
     >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-toolbar-title>
+          <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+          <span v-text="title"></span>
+      </v-toolbar-title>
     </v-toolbar>
     <v-content>
       <router-view/>
@@ -50,14 +65,31 @@
         cordova: Vue.cordova,
         clipped: false,
         drawer: true,
-        items: [{
-          icon: 'bubble_chart',
-          title: 'Inspire'
-        }],
+
+        items: [
+            {
+                icon: 'store',
+                title: 'Centrum'
+            },
+            {
+                icon: 'store',
+                title: 'Biedronka'
+            },
+            {
+                icon: 'store',
+                title: 'Unimet'
+            },
+            {
+                icon: 'store',
+                title: 'Apteka'
+            },
+        ],
+
         miniVariant: false,
         right: true,
         rightDrawer: false,
-        title: 'Vuetify.js'
+        title: 'Vuetify.js',
+        fixed: true
       }
     },
     created () {
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,17 +1,89 @@
 <template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="../../static/img/v.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card color="primary">
+          <v-card-text class="px-0 py-0 title white--text">
+              <v-subheader inset dark class="title white--text center-xs-text">Biedronka</v-subheader>
+          </v-card-text>
+          <v-list two-line subheader>
+            <v-list-tile avatar>
+              <v-list-tile-action>
+                <v-checkbox v-model="notifications"></v-checkbox>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>Notifications</v-list-tile-title>
+                <v-list-tile-sub-title>Allow notifications</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile avatar>
+              <v-list-tile-action>
+                <v-checkbox v-model="sound"></v-checkbox>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>Sound</v-list-tile-title>
+                <v-list-tile-sub-title>Hangouts message</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile avatar>
+              <v-list-tile-action>
+                <v-checkbox v-model="video"></v-checkbox>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>Video sounds</v-list-tile-title>
+                <v-list-tile-sub-title>Hangouts video call</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile avatar>
+              <v-list-tile-action>
+                <v-checkbox v-model="invites"></v-checkbox>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>Invites</v-list-tile-title>
+                <v-list-tile-sub-title>Notify when receiving invites</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+
+          <v-divider light></v-divider>
+          <v-subheader inset dark class="title white--text center-xs-text">Kupione</v-subheader>
+
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-checkbox v-model="notifications"></v-checkbox>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title class="striked grey--text">Notifications</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-checkbox v-model="sound"></v-checkbox>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title class="striked grey--text">Sound</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+
+        </v-card>
+        <v-btn
+            bottom
+            right
+            fab
+            dark
+            fixed color="teal">
+          <v-icon dark>reorder</v-icon>
+        </v-btn>
+
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
+
+<style lang="stylus">
+.striked {
+    text-decoration: line-through;
+}
+</style>
