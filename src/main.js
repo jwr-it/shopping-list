0231ba2a5374b54221a3d6d6f86bfbe50c64ed0f
Dominik Jawor <jawor@nonstop.co>
2018-03-03 12:18:45 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
+import VueCordova from 'vue-cordova'
+import VueHead from 'vue-head'
+
+import App from './App'
+import router from './router'
+
+Vue.use(Vuetify)
+Vue.config.productionTip = false
+Vue.use(VueCordova)
+Vue.use(VueHead)
+
+// add cordova.js only if serving the app through file://
+if (window.location.protocol === 'file:' || window.location.port === '3000') {
+  var cordovaScript = document.createElement('script')
+  cordovaScript.setAttribute('type', 'text/javascript')
+  cordovaScript.setAttribute('src', 'cordova.js')
+  document.body.appendChild(cordovaScript)
+}
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
